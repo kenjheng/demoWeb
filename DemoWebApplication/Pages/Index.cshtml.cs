3ef26af10401a05d93567a75cfe71fafbd139a9f
ken_jheng <ken.jheng@mile.cloud>
2023-09-07 06:20:11 +0000

    code for demo

---
+++ DemoWebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DempWebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<ClientInfo> listClients = new List<ClientInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                String connectonString = "Data Source=INSTANCE-2\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectonString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.create_at = reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public String id;
+        public String name;
+        public String email;
+        public String phone;
+        public String address;
+        public String create_at;
+    }
+}
